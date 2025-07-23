fbd719a907184dafb6b692e237ff5017af393c5b
Ivarave API <eivarave404@gmail.com>
2025-07-23 15:43:32 +0100

    settings module

--- schoolproject/wsgi.py
+++ schoolproject/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'schoolproject.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'schoolproject.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
 
 application = get_wsgi_application()
